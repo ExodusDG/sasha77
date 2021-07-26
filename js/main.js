f9b7134682e09ed0d62bb8549d0ca46d319a9cef
Nikolay <68652897+SanistRay@users.noreply.github.com>
2021-07-26 23:51:56 +0300

    5656

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.slider-carousel').slick({
-        dots: true
-    });
-});
-
-$('.card').click(function() {
-    $(this).find('.collapse').toggleClass('show')
-});
