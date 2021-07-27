18e99632662246c50e1f477ccd41ccc196477a1c
Nikolay <68652897+SanistRay@users.noreply.github.com>
2021-07-27 17:59:22 +0300

    46

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ $(document).ready(function() {
 $('.card').click(function() {
     $(this).find('.collapse').toggleClass('show')
 });
+
+$('.tariff-card').click(function() {
+    $('.tariff-card').removeClass('tariff-card-active')
+    $('.tariff-card-mounth-count').removeClass('tariff-card-mounth-count-active')
+    $('.tariff-card-mounth').removeClass('tariff-card-mounth-active')
+    $('.tariff-card-adv').removeClass('tariff-card-adv-active')
+    $('.tariff-card-discount').removeClass('card-gradient-discount-active')
+
+    $(this).addClass('tariff-card-active')
+    $(this).find('.tariff-card-mounth-count').addClass('tariff-card-mounth-count-active')
+    $(this).find('.tariff-card-mounth').addClass('tariff-card-mounth-active')
+    $(this).find('.tariff-card-adv').addClass('tariff-card-adv-active')
+    $(this).find('.tariff-card-discount').addClass('card-gradient-discount-active')
+})
